916de8da51dbb981196cf52d012a5acc3cc3aa4c
Lawrence <Lawrence4code@gmail.com>
2017-02-04 08:02:51 +0530

    Heroku app deloy attempt2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_ORANGE_URI || 'mongodb://localhost:30823/TodoApp');
 
 module.exports = { mongoose };
